fb8e7b58c1a82527d42c8b6fb4df09e8abdd282d
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-11-19 13:14:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians, instruments){
+  var beatlesPlay = new Array();
+  for (var i=0; i<4; i++){
+  beatlesPlay[i] = `${musicians[i]} plays ${instruments[i]}`
+  }
+return beatlesPlay;
+}
+
+function johnLennonFacts(facts){
+let beatles = 0
+  while (beatles<facts.length){
+    facts[beatles]=`${facts[beatles]}!!!`
+    beatles++
+  }
+  return facts
+}
+
+function iLoveTheBeatles(number){
+  var love = new Array();
+
+  do {
+    love[number]= `I love the Beatles!`
+number ++
+  }while (number > 15)
+  return love
+}
